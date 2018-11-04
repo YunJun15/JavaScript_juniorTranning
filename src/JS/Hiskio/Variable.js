4d21c342e60d6111f89814cad3d96d298045209f
joyce.z <joyce.z@my-cpg.com>
2018-11-04 22:37:46 +0800

    variable homeWork

--- src/JS/Hiskio/Variable.js
+++ src/JS/Hiskio/Variable.js
@@ -1,4 +1,4 @@
-/*
+/* 作業一
 1.學號
 2.學生的姓名
 3.學生的出生年月日
@@ -7,3 +7,37 @@
 6.學生的家長(其中一位聯絡用的)
 現在要請你把這些資料的項目，寫成變數或常數的名稱，請你進行命名的工作。
  */
+const studentId
+const studentDate
+const studentTel
+const studentName
+const studentAddress
+const studentParent
+
+/*作業二
+最近公司接了一個家用遊戲代理商妥託的案子，希望能作一個網站上的JavaScript應用程式，把遊戲公司目前代理的遊戲軟體，放到網路上提供給他們的客戶了解。
+經過調查和討論後，遊戲大致會有以下幾個資料欄位:
+
+1.遊戲的發行編號(例如: KK123)
+2.遊戲名稱
+3.遊戲的製作公司
+4.遊戲的發行日期
+5.遊戲支援的語言(例如: 日文、中文、英文...)
+6.遊戲支援的平台(例如: PS4, Xbox...)
+7.遊戲的種類(例如: 動作類、射擊類)
+8.遊戲是否為18禁
+9.遊戲是否需要網路連線
+現在要請你把這些資料的項目，寫成變數或常數的名稱，請你進行命名的工作。
+ */
+let gameLimit
+let gameConnection
+const gameId
+const gameLangurage
+const gameSupport
+const gameType
+const gameName
+const gameCompany
+const gameIssueDate
+
+// 作業三
+ 
